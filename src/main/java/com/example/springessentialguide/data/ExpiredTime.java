4a15b3e4f68787f8875765dec1843a8c9d7c4ac9
lusate <rayrayr@naver.com>
2024-08-17 13:33:35 +0900

    Feat - basic-JWT Practice

--- src/main/java/com/example/springessentialguide/data/ExpiredTime.java
+++
@@ -1,6 +0,0 @@
-package com.example.springessentialguide.data;
-
-public class ExpiredTime {
-    public static final int ACCESS_TOKEN_EXPIRE_TIME = 86400;
-    public static final int REFRESH_TOKEN_EXPIRE_TIME = 86400;
-}
