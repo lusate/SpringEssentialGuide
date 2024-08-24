da7247df9b0e60c6712d25145695d52c34da590a
lusate <rayrayr@naver.com>
2024-08-24 15:49:15 +0900

    [Feat] - complete yumi 10
    OAuth2가 제대로 완성이 되면 그때 JWT-Deeper와 merge

--- src/main/java/com/example/springessentialguide/data/dto/CustomOAuth2User.java
+++ src/main/java/com/example/springessentialguide/data/dto/CustomOAuth2User.java
@@ -33,12 +33,10 @@ public class CustomOAuth2User implements OAuth2User {
 
     @Override
     public String getName() {
-
         return userDto.getName();
     }
 
     public String getUsername() {
-
         return userDto.getUsername();
     }
 }
