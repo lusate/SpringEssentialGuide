40f4dfa15c4c3be662a8c7951ed1b3fbe2977b6a
lusate <rayrayr@naver.com>
2024-09-04 10:26:10 +0900

    CORS 추가

--- src/main/java/com/example/springessentialguide/config/SecurityConfig.java
+++ src/main/java/com/example/springessentialguide/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import com.example.springessentialguide.jwt.CustomLogoutFilter;
 import com.example.springessentialguide.jwt.JWTFilter;
 import com.example.springessentialguide.jwt.JWTUtil;
 import com.example.springessentialguide.jwt.LoginFilter;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,6 +18,10 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.logout.LogoutFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Collections;
 
 @Configuration
 @EnableWebSecurity
@@ -40,7 +45,32 @@ public class SecurityConfig {
                 .sessionManagement((session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .addFilterBefore(new JWTFilter(jwtUtil), LoginFilter.class)
                 .addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration), jwtUtil, refreshRepository), UsernamePasswordAuthenticationFilter.class)
-                .addFilterBefore(new CustomLogoutFilter(jwtUtil, refreshRepository), LogoutFilter.class);
+                .addFilterBefore(new CustomLogoutFilter(jwtUtil, refreshRepository), LogoutFilter.class)
+
+                .cors(corsCustomizer -> corsCustomizer.configurationSource(new CorsConfigurationSource() {
+
+                    @Override
+                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+
+                        CorsConfiguration configuration = new CorsConfiguration();
+
+                        configuration.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
+                        // 허용할 HTTP 메서드를 설정합니다. *로 설정하면 모든 메서드(GET, POST, PUT, DELETE 등)를 허용합니다.
+                        configuration.setAllowedMethods(Collections.singletonList("*"));
+                        // 클라이언트가 자격 증명(예: 쿠키, HTTP 인증)을 포함할 수 있도록 허용합니다. 이 설정이 true일 경우, allowedOrigins에 명시된 출처만 사용할 수 있습니다.
+                        configuration.setAllowCredentials(true);
+                        // 요청에서 허용할 헤더를 설정합니다. *로 설정하면 모든 헤더를 허용합니다.
+                        configuration.setAllowedHeaders(Collections.singletonList("*"));
+                        configuration.setMaxAge(3600L); // CORS preflight 요청의 캐시 시간을 설정합니다. 여기서는 3600초(1시간) 동안 캐시됩니다.
+
+                        // Set-Cookie 헤더를 클라이언트에서 접근할 수 있도록 노출합니다.
+                        configuration.setExposedHeaders(Collections.singletonList("Set-Cookie"));
+                        // Authorization 헤더도 클라이언트에서 접근할 수 있도록 노출합니다.
+                        configuration.setExposedHeaders(Collections.singletonList("Authorization"));
+
+                        return configuration;
+                    }
+                }));
 
         return http.build();
     }
